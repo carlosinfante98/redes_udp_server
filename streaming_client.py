1c4441a457beb8f469e076ceb0c40c6b3e2b942b
Sergio <sergiocanalesm1@gmail.com>
2020-04-08 18:39:25 -0500

    streaming funcionando

--- streaming_client.py
+++ streaming_client.py
@@ -1,3 +1,4 @@
+'''
 import socket
 import time
 import struct
@@ -53,8 +54,8 @@ def play(filename):
 
 def main():
     buffersize = 1024
-    MCAST_GRP = '224.1.1.1'
-    MCAST_PORT = 5007
+    MCAST_GRP = '224.1.1.2'
+    MCAST_PORT = 5008
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((MCAST_GRP, MCAST_PORT))
@@ -65,3 +66,39 @@ def main():
 
 if __name__ == '__main__':
     main()
+    '''
+import socket
+import numpy as np
+import cv2 as cv
+
+
+addr = ("127.0.0.1", 65534)
+buf = 512
+width = int(640*1.5)
+height = int(480*1.5)
+code = b'start'
+num_of_chunks = width * height * 3 / buf
+
+if __name__ == '__main__':
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(addr)
+    while True:
+        chunks = []
+        start = False
+        while len(chunks) < num_of_chunks:
+            chunk, _ = s.recvfrom(buf)
+            if start:
+                chunks.append(chunk)
+            elif chunk.startswith(code):
+                start = True
+
+        byte_frame = b''.join(chunks)
+
+        frame = np.frombuffer(byte_frame, dtype=np.uint8).reshape(height, width, 3)
+
+        cv.imshow('recv', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    s.close()
+    cv.destroyAllWindows()
--- streaming_server.py
+++ streaming_server.py
@@ -96,23 +96,36 @@ if __name__ == '__main__':
 #vs.stop()
 cap.release()
 '''
-from flask import Flask, render_template, Response
-from camera import Camera
+import socket
+import numpy as np
+import cv2 as cv
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+addr = ("127.0.0.1", 65534)
+buf = 512
+width = 640
+height = 480
+cap = cv.VideoCapture('./media/small.mp4')
+#cap.set(3, width)
+#cap.set(4, height)
+code = 'start'
+code = ('start' + (buf - len(code)) * 'a').encode('utf-8')
 
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret:
+            s.sendto(code, addr)
+            data = frame.tostring()
+            for i in range(0, len(data), buf):
+                s.sendto(data[i:i+buf], addr)
+            # cv.imshow('send', frame)
+            # if cv.waitKey(1) & 0xFF == ord('q'):
+                # break
+        else:
+            break
+    # s.close()
+    # cap.release()
+    # cv.destroyAllWindows()
